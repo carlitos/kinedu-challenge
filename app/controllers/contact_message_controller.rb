9263fe35d68f91f2d037d1077a9bd2cd351d01c4
carlitos <carmoicast@gmail.com>
2020-08-06 02:21:29 -0500

    Corregir metodo list para que regresara el id, agregar dos metodos mas

--- app/controllers/contact_message_controller.rb
+++ app/controllers/contact_message_controller.rb
@@ -1,10 +1,9 @@
-
 class ContactMessageController
 
-    def self.all
+    def self.list
         items = ContactMessages.scan()
         items
-          .map { |r| { :name => r.name, :email => r.email, :context => r.context } }
+          .map { |r| { :id => r.id, :name => r.name, :email => r.email, :context => r.context } }
           .sort { |a, b| a[:created_at] <=> b[:created_at] }
           .to_json
     end
@@ -17,5 +16,17 @@ class ContactMessageController
         item.save!
         item
     end
+
+    def self.get_by_id(params)
+      
+      item.id = params.id
+      item = ContactMessages.find(item.id)
+      item.to_json
+    end
+
+    # def self.find_by_email(params)
+    #   item = ContactMessages.Book.find(:all, :where => 'email = "nrubio@congresogto.gob.mx"')
+    #   item.to_json
+    # end
 
 end
