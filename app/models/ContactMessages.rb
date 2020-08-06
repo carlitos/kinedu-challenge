d1868e05a1cbb80ebc78d763801c3cee5ad7156a
carlitos <carmoicast@gmail.com>
2020-08-06 02:19:36 -0500

    Renombrar contact messages controller

--- app/models/ContactMessages.rb
+++
@@ -1,9 +0,0 @@
-class ContactMessages
-    include Aws::Record
-    string_attr :id, hash_key: true
-    string_attr :email
-    string_attr :name
-    string_attr :context
-    epoch_time_attr :created_at
-    epoch_time_attr :updated_at
-  end
